655ce3c51f70a090da8819f2c4d4a3a7518202f7
pei <pei.shi@outlook.com>
2018-08-23 00:32:05 +0200

    refactory, injection

--- 03-IoC-DI/injection/src/main/java/com/apress/prospring5/ch3/xml/InjectRef.java
+++ 03-IoC-DI/injection/src/main/java/com/apress/prospring5/ch3/xml/InjectRef.java
@@ -3,21 +3,16 @@ package com.apress.prospring5.ch3.xml;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import com.apress.prospring5.ch3.Oracle;
 
+import lombok.Setter;
+import pei.java.spring.lab.utils.Utils;
+
 public class InjectRef {
+    @Setter
     private Oracle oracle;
 
-    public void setOracle(Oracle oracle) { 
-        this.oracle = oracle;
-    }
-
     public static void main(String... args) {
-        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
-        ctx.load("classpath:spring/app-context-xml.xml");
-        ctx.refresh();
-
-        InjectRef injectRef = (InjectRef) ctx.getBean("injectRef");
-        System.out.println(injectRef);
-
+        GenericXmlApplicationContext ctx = Utils.getGenericXmlAppCtx("classpath:spring/app-context-xml.xml");
+        System.out.println((InjectRef) ctx.getBean("injectRef"));
         ctx.close();
     }
 
